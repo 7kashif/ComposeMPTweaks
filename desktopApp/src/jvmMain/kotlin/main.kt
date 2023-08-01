7064c691a3a4fc679a7a310b5cb6fccf00b20c21
Kashif Masood <khanzada.kashif2001@gmail.com>
2023-08-01 23:49:57 +0500

    Desktop app improvement.

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,9 +1,16 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.kashif.composemptweaks.MainView
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-       MainView()
+    val windowState = rememberWindowState()
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = windowState,
+        title = "ComposeMpTweaks"
+    ) {
+        MainView()
     }
 }
